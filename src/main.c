fae2e82eb8c57452b296c25d524f4add62efd174
Tristan Thomas <tkthomas@student.unimelb.edu.au>
2023-04-07 15:23:59 +1000

    Reads in command line args

--- src/main.c
+++ src/main.c
@@ -6,9 +6,36 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 
-int main(int argc, char **argv) {
+void process_args(int argc, char **argv, char *scheduler, char *mem_strategy, int *quantum, FILE **f);
 
-    printf("Hello World!2\n");
+int main(int argc, char *argv[]) {
+    int quantum;
+    char *scheduler = NULL, *mem_strategy = NULL;
+    FILE *input_file = NULL;
+
+    // process command line arguments
+    process_args(argc, argv, scheduler, mem_strategy, &quantum, &input_file);
+
+    free(scheduler);
+    free(mem_strategy);
+    fclose(input_file);
+
     return 0;
+}
+
+void process_args(int argc, char **argv, char *scheduler, char *mem_strategy, int *quantum, FILE **f) {
+    scheduler = (char *) malloc(strlen(argv[4]));
+    assert(scheduler);
+    mem_strategy = (char *) malloc(strlen(argv[6]));
+    assert(mem_strategy);
+    *f = fopen(argv[2], "w");
+    assert(f);
+
+    strcpy(scheduler, argv[4]);
+    strcpy(mem_strategy, argv[6]);
+    *quantum = atoi(argv[8]);
+
 }
